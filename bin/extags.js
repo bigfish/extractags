5e0b11e6bfad0e6eec501594e0ad05c4b9b054ab
David Wilhelm <dewilhelm@gmail.com>
2011-07-08 21:45:33 -0400

    refactored bin/extags to use new cli.js

--- bin/extags.js
+++ bin/extags.js
@@ -1,28 +1,5 @@
 #!/usr/bin/env node
 
 /*jslint node:true */
-(function () {
-    var args, classes, fs, parser, topdir;
-    fs = require('fs');
-    args = process.argv;
-    if (args.length !== 3) {
-        console.log("missing required directory argument");
-        process.exit(1);
-    }
-    topdir = args.pop();
-    parser = require("../lib/extjs4ctags");
-    fs.stat(topdir, function (err, stats) {
-        if (err) {
-            throw (err);
-        }
-        if (stats.isFile()) {
-            parser.parseFile(topdir, function (err, classData) {
-                console.dir(classData);
-            });
-        } else if (stats.isDirectory) {
-            parser.parseDir(topdir, function (err, classes) {
-                console.dir(classes);
-            });
-        }
-    });
-}());
+cli = require('../lib/cli/cli.js');
+cli();
