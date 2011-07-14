3201c5468317af9f6002114c88458c6bf8443d1f
David Wilhelm <dewilhelm@gmail.com>
2011-07-13 21:23:40 -0400

    parsing params of constructor function

--- spec/parse_spec.js
+++ spec/parse_spec.js
@@ -4,13 +4,78 @@
     describe("parse Ext JS 4 source files", function () {
         var extags, fileText;
         extags = null;
-        fileText = "Ext.define(\"some.Class\", {\n    \n});";
 
         it('should parse the class name from a String', function () {
+            fileText = "Ext.define(\"some.Class\", {\n    \n});";
             var parsed_class;
             extags = require('../lib/parser');
             parsed_class = extags.parseText(fileText);
             expect(parsed_class.fullClassName).toBe("some.Class");
+        });
+
+        it('should parse the constructor function with no params', function () {
+            fileText = ["Ext.define(\"some.Class\", {",
+                                                                                                                                                                                                        "constructor: function () {",
+                                                                                                                                                                                                        "}",
+                                                                                                                                                                                                        "});"].join("\n");
+            var parsed_class;
+            extags = require('../lib/parser');
+            parsed_class = extags.parseText(fileText);
+            expect(parsed_class.constructorFn.params.length).toBe(0);
+        });
+        it('should parse the constructor function with 1 param', function () {
+            fileText = ["Ext.define(\"some.Class\", {",
+                    "constructor: function (arg) {",
+                    "this.foo = arg;",
+                    "}",
+                    "});"].join("\n");
+            var parsed_class;
+            extags = require('../lib/parser');
+            parsed_class = extags.parseText(fileText);
+            expect(parsed_class.constructorFn.params.length).toBe(1);
+            expect(parsed_class.constructorFn.params[0]).toBe('arg');
+        });
+        it('should parse the constructor function with multiple param', function () {
+            fileText = ["Ext.define(\"some.Class\", {",
+                                            "constructor: function (arg1, arg2) {",
+                                            "this.foo = arg1;",
+                                            "this.bar = arg2;",
+                                            "}",
+                                            "});"].join("\n");
+            var parsed_class;
+            extags = require('../lib/parser');
+            parsed_class = extags.parseText(fileText);
+            expect(parsed_class.constructorFn.params.length).toBe(2);
+            expect(parsed_class.constructorFn.params[0]).toBe('arg1');
+            expect(parsed_class.constructorFn.params[1]).toBe('arg2');
+        });
+        it('should parse the constructor function with multiple params and exclude comments', function () {
+            fileText = ["Ext.define(\"some.Class\", {",
+                                "constructor: function (arg1, arg2/*foo*/) {",
+                                "this.foo = arg1;",
+                                "this.bar = arg2;",
+                                "}",
+                                "});"].join("\n");
+            var parsed_class;
+            extags = require('../lib/parser');
+            parsed_class = extags.parseText(fileText);
+            expect(parsed_class.constructorFn.params.length).toBe(2);
+            expect(parsed_class.constructorFn.params[0]).toBe('arg1');
+            expect(parsed_class.constructorFn.params[1]).toBe('arg2');
+        });
+        it('should parse the constructor function with multiple params and exclude multiple comments', function () {
+            fileText = ["Ext.define(\"some.Class\", {",
+                                "constructor: function (/*some comment*/arg1,/*another comment*/ arg2/*foo*/) {",
+                                "this.foo = arg1;",
+                                "this.bar = arg2;",
+                                "}",
+                                "});"].join("\n");
+            var parsed_class;
+            extags = require('../lib/parser');
+            parsed_class = extags.parseText(fileText);
+            expect(parsed_class.constructorFn.params.length).toBe(2);
+            expect(parsed_class.constructorFn.params[0]).toBe('arg1');
+            expect(parsed_class.constructorFn.params[1]).toBe('arg2');
         });
 
     });
