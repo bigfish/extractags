ccf58f07cc45086181201c6cfa1912a3ef798863
David Wilhelm <dewilhelm@gmail.com>
2011-07-20 20:29:34 -0400

    watcher sees files being deleted

--- spec/watcher_spec.js
+++ spec/watcher_spec.js
@@ -3,6 +3,7 @@
 (function () {
     var fs = require('fs');
     describe("file watcher watches files", function () {
+
         it("should watch a file and call the callback when it changes", function () {
             var error, fileChanged, watcher;
             fileChanged = undefined;
@@ -20,6 +21,7 @@
                 expect(fileChanged).toBe('./spec/test_watcher_files/afile.js');
             });
         });
+
         it("should watch a dir and call the callback when a file in it changes", function () {
             var fileChanged, watcher;
             fileChanged = undefined;
@@ -38,6 +40,7 @@
                 expect(fileChanged).toBe('./spec/test_watcher_files/adir/anotherFile.txt');
             });
         });
+
         it("should watch a dir and call the callback when a file in it created", function () {
             var fileChanged, watcher;
             fileChanged = undefined;
@@ -47,7 +50,6 @@
             //watch dir
             watcher.watchFiles('./spec/test_watcher_files/adir', function (file, curr, prev) {
                 fileChanged = file;
-                console.log(file + " changed");
             });
             //wait for callback
             waitsFor(function () {
@@ -55,6 +57,26 @@
             }, "watch did not get callback after file changed", 1000);
             //create a file
             fs.writeFileSync('./spec/test_watcher_files/adir/yetAnotherFile.txt', 'foo', 'UTF-8');
+            return runs(function () {
+                expect(fileChanged).toBe('./spec/test_watcher_files/adir/yetAnotherFile.txt');
+            });
+        });
+
+        it("should watch a dir and call the callback when a file in it created", function () {
+            var fileChanged, watcher;
+            fileChanged = undefined;
+            watcher = require('../lib/watcher');
+            //ensure the file to be created exists
+            fs.writeFileSync('./spec/test_watcher_files/adir/yetAnotherFile.txt', 'foo', 'UTF-8');
+            //watch dir
+            watcher.watchFiles('./spec/test_watcher_files/adir', function (file, curr, prev) {
+                fileChanged = file;
+            });
+            //wait for callback
+            waitsFor(function () {
+                return fileChanged !== undefined;
+            }, "watch did not get callback after file changed", 1000);
+            //delete file
             return runs(function () {
                 expect(fileChanged).toBe('./spec/test_watcher_files/adir/yetAnotherFile.txt');
             });
