6b49e7ef13b3c048915cc32be5be14c06c764072
David Wilhelm <dewilhelm@gmail.com>
2011-07-20 20:26:40 -0400

    watching dirs is working for changing and adding

--- spec/watcher_spec.js
+++ spec/watcher_spec.js
@@ -20,5 +20,44 @@
                 expect(fileChanged).toBe('./spec/test_watcher_files/afile.js');
             });
         });
+        it("should watch a dir and call the callback when a file in it changes", function () {
+            var fileChanged, watcher;
+            fileChanged = undefined;
+            watcher = require('../lib/watcher');
+            //watch dir
+            watcher.watchFiles('./spec/test_watcher_files/adir', function (file, curr, prev) {
+                fileChanged = file;
+            });
+            //wait for callback
+            waitsFor(function () {
+                return fileChanged !== undefined;
+            }, "watch did not get callback after file changed", 1000);
+            //change file
+            fs.writeFileSync('./spec/test_watcher_files/adir/anotherFile.txt', 'foo', 'UTF-8');
+            return runs(function () {
+                expect(fileChanged).toBe('./spec/test_watcher_files/adir/anotherFile.txt');
+            });
+        });
+        it("should watch a dir and call the callback when a file in it created", function () {
+            var fileChanged, watcher;
+            fileChanged = undefined;
+            watcher = require('../lib/watcher');
+            //ensure the file to be created does not exist
+            fs.unlinkSync('./spec/test_watcher_files/adir/yetAnotherFile.txt');
+            //watch dir
+            watcher.watchFiles('./spec/test_watcher_files/adir', function (file, curr, prev) {
+                fileChanged = file;
+                console.log(file + " changed");
+            });
+            //wait for callback
+            waitsFor(function () {
+                return fileChanged !== undefined;
+            }, "watch did not get callback after file changed", 1000);
+            //create a file
+            fs.writeFileSync('./spec/test_watcher_files/adir/yetAnotherFile.txt', 'foo', 'UTF-8');
+            return runs(function () {
+                expect(fileChanged).toBe('./spec/test_watcher_files/adir/yetAnotherFile.txt');
+            });
+        });
     });
 }());
